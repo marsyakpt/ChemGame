4ffb327db93474d0db4e18be4a3fccd94468c55c
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 15:17:59 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="Beranda", page_icon="🏠")
 st.title("🏠 Beranda")
 st.subheader("Selamat datang di Game & Materi Kimia! 🎉")
-st.write("Pilih halaman yang ingin kamu jelajahi:")
 
 st.page_link("pages/Kimia_Organik.py", label="🔬 Kimia Organik")
 st.page_link("pages/Kimia_Anorganik.py", label="⚗️ Kimia Anorganik")
