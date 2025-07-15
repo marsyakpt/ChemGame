d453d7cddf488d641a23f7dc5abd5a148cb1e49e
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 13:59:29 +0700

    Update your_app.py

--- your_app.py
+++ your_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("Halo! Selamat Datang di Game Kimia! 🎉")
 st.subheader("Kamu mau ngapain dulu nih? Pilih halaman:")
 
-st.page_link("your_app.py", label="🏠 Home")
 st.page_link("pages/page_1.py", label="🔬 Kimia Organik")
 st.page_link("pages/page_2.py", label="⚗️ Kimia Anorganik")
