0036856faca939e9f3469482e6d443a6c74e602a
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-19 16:08:28 +0700

    Update Tentang_Web.py

--- pages/Tentang_Web.py
+++ pages/Tentang_Web.py
@@ -1,15 +1,4 @@
 import streamlit as st
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
-
-# Setup akses Google Sheets
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-client = gspread.authorize(creds)
-
-# Buka Sheet pakai ID 
-sheet = client.open_by_key("173bLLhKkH4n0R9HVc4h1ySbXPtp_edpjLPw8DPsb-lg").sheet1  # Ganti dengan ID Sheet 
 
 # Header & Deskripsi
 st.set_page_config(page_title="Tentang CHiQ", page_icon="📌")
@@ -26,15 +15,3 @@ CHiQ adalah aplikasi kuis kimia interaktif yang dibangun untuk membuat pembelaja
 
 st.markdown("---")
 st.header("💬 Kotak Saran")
-
-# Input user
-nama = st.text_input("🧪 Nama kamu (opsional)")
-saran = st.text_area("💡 Tulis saran, ide, atau harapan kamu buat CHiQ")
-
-if st.button("Kirim Saran"):
-    if saran.strip():
-        waktu = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        sheet.append_row([waktu, nama, saran])
-        st.success("✅ Saran kamu sudah masuk ke lab CHiQ! Terima kasih atas kontribusinya 💌")
-    else:
-        st.warning("⚠️ Eh, sarannya belum ditulis nih. Yuk diisi dulu 😅")
